dae848f392c683c1c68b0b3aa396cb74c9f1e4dd
Aniket <bharambeaniket4@gmail.com>
2025-01-02 01:42:02 +0530

    Second smallest Element in array

--- Arrays_dsa/SecondLargestElementInArray/src/App.java
+++ Arrays_dsa/SecondLargestElementInArray/src/App.java
@@ -2,12 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int[] arr = {3,2,1,5,4};
+        int[] arr = {3,2,1,0,5,4};
         secondLargestElementArray(arr);
+        secondSmallestElementArray(arr);
     }
 
     public static void secondLargestElementArray(int[] arr){
-
         int largestElement = arr[0];
         int secondLargestElement = -1;
 
@@ -22,4 +22,21 @@ public class App {
         }
         System.out.println("Second Largest :"+ secondLargestElement);
     }
+
+    public static void secondSmallestElementArray(int[] arr){
+        int smallestElement = arr[0];
+        int secondSmallestElement = 999;
+
+        for(int i=0; i<arr.length; i++){
+            if( arr[i] < smallestElement ){
+                secondSmallestElement = smallestElement;
+                smallestElement = arr[i];
+            }else if( arr[i] > smallestElement && arr[i] < secondSmallestElement){
+                secondSmallestElement = arr[i];
+            }
+        }
+        System.out.println("Second smallest :"+ secondSmallestElement);
+    }
+
 }
+
