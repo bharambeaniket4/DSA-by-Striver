5ffbeacaf5935e00a3380ffa1a1b57369540c3fb
Aniket <bharambeaniket4@gmail.com>
2025-01-02 00:44:14 +0530

    Largest Element in Array

--- Arrays_dsa/LargetNumberInArray/src/App.java
+++ Arrays_dsa/LargetNumberInArray/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int[] arr = {3,2,1,5,2};
+        largestElementInArray( arr );
+    }
+
+    public static  void largestElementInArray(int[] arr){
+        int largestElement = arr[0];
+
+        for( int i=0; i<arr.length; i++){
+            if( arr[i] > largestElement){
+                largestElement = arr[i];
+            }
+        }
+        System.out.println( "LargestElement :"+ largestElement );
+
     }
 }
